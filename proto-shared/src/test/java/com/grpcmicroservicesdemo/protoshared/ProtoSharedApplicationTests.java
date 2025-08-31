2e40d2de377da9846d1b9201b33fe152f73ef150
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2025-08-31 13:00:45 +0530

    Initialize project

---
+++ proto-shared/src/test/java/com/grpcmicroservicesdemo/protoshared/ProtoSharedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grpcmicroservicesdemo.protoshared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoSharedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
